7cb7350f7a05a848b42ff340433d42c9756cd930
____marcell <0000marcell@gmail.com>
2019-11-19 20:17:49 +0000

    saveShelfTemplate added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const getAuthCookie = require('./src/get-auth-cookie');
 const saveTemplate = require('./src/save-template');  
 const saveSubtemplate = require('./src/save-subtemplate');
+const saveShelfTemplate = require('./src/save-shelf-template');
 
 module.exports = {
   getAuthCookie: getAuthCookie,
   saveTemplate: saveTemplate,
-  saveSubtemplate: saveSubtemplate
+  saveSubtemplate: saveSubtemplate,
+  saveShelfTemplate: saveShelfTemplate
 }
