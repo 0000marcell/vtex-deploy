606938b161e69d4b6b18ea035f8f43d38f9193e2
zeonardo <zeonardo@gmail.com>
2020-01-22 11:09:35 -0300

    z: add get-templates-ids to the available methods

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const getAuthCookie = require('./src/get-auth-cookie');
-const saveTemplate = require('./src/save-template');  
+const getTemplateIds = require('./src/get-templates-ids');
+const saveTemplate = require('./src/save-template');
 const saveSubtemplate = require('./src/save-subtemplate');
 const saveShelfTemplate = require('./src/save-shelf-template');
 
 module.exports = {
   getAuthCookie: getAuthCookie,
+  getTemplateIds: getTemplateIds,
   saveTemplate: saveTemplate,
   saveSubtemplate: saveSubtemplate,
   saveShelfTemplate: saveShelfTemplate
-}
+};
