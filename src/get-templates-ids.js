e2ca1c16b682213e170afe24de1b04fafad6cba4
zeonardo <zeonardo@gmail.com>
2020-01-22 11:05:42 -0300

    z: updating error handling and messages for get-templates-ids

--- src/get-templates-ids.js
+++ src/get-templates-ids.js
@@ -3,8 +3,8 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 module.exports = async function(opts, cb) {
-  type = opts.type || 'viewTemplate';
-  isSub = opts.isSub || false;
+  let type = opts.type || 'viewTemplate';
+  let isSub = opts.isSub || false;
   let baseUrl = `.vtexcommercestable.com.br/admin/a/PortalManagement/GetTemplateList?type=${type}&IsSub=${isSub ? 1 : 0}`;
   let url = `https://${opts.store}${baseUrl}`;
   try {
@@ -18,22 +18,39 @@ module.exports = async function(opts, cb) {
       method: 'POST',
       url: url
     }, function(error, response, body){
+      //TODO: 
       if(error) {
-        console.error('Error when trying to retrieve template list', error);
+        console.error(`Error trying to retrieve ${type} list`, error);
+      }
+      if((response.statusCode > 399 && response.statusCode < 601)) {
+        console.error(`* error getting ${type} ids (${response.statusCode})`);
+      }
+      if(/\/admin\/login\?portal/.test(body)){
+        console.warn(`* invalid or expired cookie (${response.statusCode})`);
       }
       const dom = new JSDOM(`<!DOCTYPE html>${body}`);
-      let ul = dom.window.document.querySelector("ul");
       let results = {};
-      ul.querySelectorAll("li").forEach((item) => {
-        let name = item.querySelector('div').textContent;
-        let id = item.querySelector('a')
-          .attributes['href'].textContent.match(/templateId=(.*)/)[1];
-        results[name] = id;
-      });
+      let ul = dom.window.document.querySelector("ul");
+      if(ul == null){
+        console.error(`* no valid ${type} ids to show`);
+      }
+      else{
+        ul.querySelectorAll("li").forEach((item) => {
+          let name = item.querySelector('div').textContent;
+          let id = item.querySelector('a')
+            .attributes['href'].textContent.match(/templateId=(.*)/)[1];
+          results[name] = id;
+        });
+      }
 
-      cb(results);
+      if(typeof cb == 'function'){
+        cb(results);
+      }
     });
   } catch(err) { 
     console.error(err);
+    if(typeof cb == 'function'){
+      cb({});
+    }
   }
 }
